74481ea4e8fb6071ae4c0e43b7c1ec52a39b0121
zhangxiaochu001 <zhangxiaochu001@gmail.com>
2019-12-28 11:48:14 +0800

    refactor

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -12,12 +12,32 @@ public class FizzBuzzTest {
     }
 
     @org.junit.Test
-    public void test() {
+    public void test0() {
         Assert.assertEquals("FizzBuzz", FizzBuzz.isFizzBuzz(15, 3, 5));
+    }
+
+    @org.junit.Test
+    public void test1() {
         Assert.assertEquals("Fizz", FizzBuzz.isFizzBuzz(3, 3, 5));
+    }
+
+    @org.junit.Test
+    public void test2() {
         Assert.assertEquals("Buzz", FizzBuzz.isFizzBuzz(5, 3, 5));
+    }
+
+    @org.junit.Test
+    public void test3() {
         Assert.assertEquals("1", FizzBuzz.isFizzBuzz(1, 3, 5));
+    }
+
+    @org.junit.Test
+    public void test4() {
         Assert.assertEquals("FizzBuzz", FizzBuzz.isFizzBuzz(0, 3, 5));
+    }
+
+    @org.junit.Test
+    public void test5() {
         Assert.assertEquals("11", FizzBuzz.isFizzBuzz(11, 3, 5));
     }
 }
